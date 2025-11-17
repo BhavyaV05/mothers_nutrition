2932fb5dfccce3e8068647875047ffaa5a875368
Atharv Johar <joharatharv@gmail.com>
2025-11-17 18:46:44 +0530

    finish

--- db_setup/routes/mothers.routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { registerMother } from "../controllers/mothers.controller.js";
-
-const router = express.Router();
-
-router.post("/", registerMother);
-
-export default router;
